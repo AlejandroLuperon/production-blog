bb67c9c3ffe8b526cc5ebed4de4841a79e787038
Alejandro Luperon <alejandro.luperon@gmail.com>
2018-09-03 12:13:53 -0400

    Updating about section.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,8 @@ import me from "../images/me.jpg";
 export default () => (
   <div>
     <h1>Build Whatever You Want</h1>
-    {/*<img style={{maxHeight: '500px'}} src={me}/>*/}
     <p>
-      This is a blog run by <a target='_blank' href="https://www.linkedin.com/in/alejandroluperon/">me</a>, Alejandro Luperon. The topics in this blog will range across a variety of topics in Software Engineering,
+      This is a blog run by <a target='_blank' href="https://www.linkedin.com/in/alejandroluperon/">me</a>, Alejandro Luperon. The topics in this blog will range across a variety of topics in Software Engineering
       ranging from Front-End, to Back-End, database development, API development, machine learning, and more. I really enjoy engineering strategy, organization,
       and methodology, so there will also be articles about that as well.
     </p>
@@ -15,6 +14,6 @@ export default () => (
       I want people to feel comfortable entering and staying in the engineering world without insecurity or fear, so that way they can use their own positive traits to
       participate and grow however they see fit.
     </p>
-    <p>This blog was built using Gatsby.js and React, and is under active construction.</p>
+    <p>This blog was built using Gatsby.js and React, deployed on AWS, and is under active construction.</p>
   </div>
 );
