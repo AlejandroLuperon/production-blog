89030d01f998e08bab98a29038d2b88153cbd94e
Alejandro Luperon <alejandro.luperon@gmail.com>
2019-01-10 12:48:16 -0500

    Adding social icons to the banner component.

--- src/common/banner/banner.js
+++ src/common/banner/banner.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Twitter from "../icons/Twitter.js";
+import Instagram from "../icons/Instagram.js";
+import './banner.css'
 
 class Banner extends React.Component {
   constructor(props) {
@@ -6,8 +9,16 @@ class Banner extends React.Component {
   }
   render() {
     return (
-      <div style={{border: '2px solid #e6e6e6', padding: '10px'}}>
-        <strong>{this.props.title}</strong> - {this.props.children}
+      <div>
+        <div style={{border: '2px solid #e6e6e6', padding: '10px'}}>
+          <strong>{this.props.title}</strong> - {this.props.children}
+        </div>
+        <div className='d-flex justify-content-center'>
+          <div className='icons'>
+            <Twitter />
+            <Instagram />
+          </div>
+        </div>
       </div>
     )
   }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import g from "glamorous";
 import { css } from "glamor";
 import Link from "gatsby-link";
 import Menu from '../common/menu/menu.js';
+import Banner from "../common/banner/banner.js";
 
 import { rhythm } from "../utils/typography";
 import './Body.css';
@@ -11,15 +12,25 @@ import './Body.css';
 export default ({ children, data }) => (
   <div>
     <Menu />
-     <div style={{
-        paddingTop: '100px',
-        paddingLeft: '20px',
-        paddingRight: '20px',
-        paddingBottom: '35px',
-        fontFamily: 'sans-serif'
-      }}>
-        {children()}
-      </div>
+    <div className='d-flex flex-column align-items-center' style={{paddingLeft: '20px',paddingRight: '20px'}}>
+      <div className='d-flex flex-md-row flex-column-reverse col-12'
+       style={{
+         maxWidth: '1200px',
+         paddingTop: '100px',
+         paddingBottom: '35px',
+         fontFamily: 'sans-serif'
+       }}>
+         <div className='col-md-8 col-12'>
+           {children()}
+         </div>
+         <div className='banner col-md-4 col-12'>
+           <Banner title="Build Whatever You Want">
+             Articles on various topics in Software Engineering and Computer Science. All articles are written by Alejandro Luperon.
+           </Banner>
+         </div>
+       </div>
+    </div>
+
   </div>
 );
 
