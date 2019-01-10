cde8c06ffd925d32f112122a894e044a1569d1af
Alejandro Luperon <alejandro.luperon@gmail.com>
2019-01-10 13:50:59 -0500

    Adding social icons.

---
+++ src/common/icons/LinkedIn.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './icons.css'
+
+class LinkedIn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <svg onClick={this.props.onClick.bind(this)} className='menu-icon' width="20" height="15" viewBox="0 0 20 15" fill="#FFFFFF" aria-hidden="true">
+          <rect width="20" height="3"></rect>
+          <rect width="20" height="3" y="6"></rect>
+          <rect width="20" height="3" y="12"></rect>
+      </svg>
+    );
+  }
+}
+
+export default LinkedIn;
